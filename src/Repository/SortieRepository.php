7172de1372059d140c9eacc471311ae3d844a08c
tonioshakka <tonioshakka@gmail.com>
2024-08-29 15:42:09 +0200

    en cours
    en cours pour vous, pour récupérer les migrations

--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -16,6 +16,64 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
+    public function findByCriteria(array $criteria, $user): array
+    {
+        $qb = $this->createQueryBuilder('s')
+            ->leftJoin('s.site', 'site')
+            ->addSelect('site');
+
+        // Filtrer par site
+        if (!empty($criteria['site'])) {
+            $qb->andWhere('site.id = :site')
+                ->setParameter('site', $criteria['site']->getId());
+        }
+
+        // Filtrer par recherche de texte dans le nom de la sortie
+        if (!empty($criteria['nom'])) {
+            $qb->andWhere('s.nom LIKE :nom')
+                ->setParameter('nom', '%' . $criteria['nom'] . '%');
+        }
+
+        // Filtrer par date de début et de fin
+        if (!empty($criteria['dateDebut'])) {
+            $qb->andWhere('s.dateHeureDebut >= :dateDebut')
+                ->setParameter('dateDebut', $criteria['dateDebut']);
+        }
+        if (!empty($criteria['dateFin'])) {
+            $qb->andWhere('s.dateHeureFin <= :dateFin')
+                ->setParameter('dateFin', $criteria['dateFin']);
+        }
+
+        // Filtrer par état
+        if (!empty($criteria['etat'])) {
+            if (in_array('organisateur', $criteria['etat'])) {
+                $qb->andWhere('s.organisateur = :user')
+                    ->setParameter('user', $user);
+            }
+
+            if (in_array('inscrit', $criteria['etat'])) {
+                $qb->innerJoin('s.inscriptions', 'i')
+                    ->andWhere('i.utilisateur = :user')
+                    ->setParameter('user', $user);
+            }
+
+            if (in_array('non_inscrit', $criteria['etat'])) {
+                $qb->leftJoin('s.inscriptions', 'i_not')
+                    ->andWhere('i_not.utilisateur IS NULL OR i_not.utilisateur != :user')
+                    ->setParameter('user', $user);
+            }
+
+            if (in_array('passees', $criteria['etat'])) {
+                $qb->andWhere('s.dateHeureFin < :now')
+                    ->setParameter('now', new \DateTime());
+            }
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+
+
     //    /**
     //     * @return Sortie[] Returns an array of Sortie objects
     //     */
